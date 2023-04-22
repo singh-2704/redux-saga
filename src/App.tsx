9def7b0717576170f995fa962d1b3c2169840c7b
saurabh singh <saurabhsingh8043@gmail.com>
2023-04-22 06:41:23 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App(){
+  return <div className="bg-red-500 font-bold">'hello world'</div>;
+} 
+
+export default App;
---
+++ src/pages/ShowListPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ShowListPage() {
+  return (
+    <div>
+      This is show List Page;
+    </div>
+  )
+}
+
+export default ShowListPage
